443c4c7cb3fe4c38779a604574f28ecdc8dbb669
Hanne Maes <maes.hanne@gmail.com>
2020-02-10 18:07:24 +0100

    added an animating arrow

--- docs/script.js
+++ docs/script.js
@@ -3,6 +3,13 @@
 
 function documentReady() {
 
+  // scroll down on arrow click
+  document.getElementById('arrowClick').onclick = function() {
+    let pageHeight = window.innerHeight;
+    // window.scrollBy(0, pageHeight, 'smooth');
+    window.scrollBy({ top: pageHeight, left: 0, behavior: "smooth" });
+  };
+
   // smooth scrolling to anchor points
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
